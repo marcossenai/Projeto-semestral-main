003dc2814608b21eeaa54daee5b150b032f81367
marcossenai <133662787+marcossenai@users.noreply.github.com>
2023-11-13 17:00:24 -0300

    mudanças

--- Projeto-semestral-overwatch-main/Página inicial/parallaxWidowmaker.js
+++ Projeto-semestral-overwatch-main/Página inicial/parallaxWidowmaker.js
@@ -3,8 +3,11 @@ const windowImage = document.querySelector('.window');
 
 tela.addEventListener('mousemove', (e) => {
     const xOffset = (e.clientX / tela.offsetWidth) * 40; 
-    const yOffset = (e.clientY / tela.offsetHeight) * 40; 
+    const yOffset = 0; 
 
     windowImage.style.transform = `translate(${xOffset}px, ${yOffset}px)`;
 });
 
+tela.addEventListener('mouseleave', () => {
+    windowImage.style.transform = 'translate(0, 0)';
+});
