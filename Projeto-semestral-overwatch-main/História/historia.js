0963c6a86eeaa397268662b2e3d19061f665a6ea
marcosSenai <marcos.lacerda374@gmail.com>
2023-12-04 23:54:27 -0300

    Atualização da página 'história'

--- Projeto-semestral-overwatch-main/História/historia.js
+++ Projeto-semestral-overwatch-main/História/historia.js
@@ -1,16 +1,27 @@
 document.querySelectorAll('#minhaLista > li').forEach(function (li) {
     li.addEventListener('click', function (e) {
-        var subLista = e.target.querySelector('ul');
+        // Remova a classe 'selecionado' de todos os elementos span
+        document.querySelectorAll('#minhaLista > li > span').forEach(function (span) {
+            span.classList.remove('selecionado');
+        });
+
+        var subLista = li.querySelector('ul');
         if (subLista && subLista.classList.contains('oculto')) {
+            // Adicione a classe 'selecionado' ao elemento span dentro do item clicado
+            var span = li.querySelector('span');
+            if (span) {
+                span.classList.add('selecionado');
+            }
+
             document.querySelectorAll('#minhaLista > li > ul').forEach(function (ul) {
                 ul.classList.add('oculto');
             });
             subLista.classList.remove('oculto');
             var imagemSrc = subLista.querySelector('li').dataset.imagem;
-            document.querySelector('#contenedorImagem').innerHTML = '<img src="' + imagemSrc + '">';
+            document.querySelector('#containerImagem').innerHTML = '<img src="' + imagemSrc + '">';
         } else if (subLista) {
             subLista.classList.add('oculto');
-            document.querySelector('#contenedorImagem').innerHTML = '';
+            document.querySelector('#containerImagem').innerHTML = '';
         }
     });
 });
